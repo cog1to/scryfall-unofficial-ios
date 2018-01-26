10eeb43062fc154d1059f39f4ffdfc5b958e5257
Alexander Rogachev <alexander.rogachev@akvelon.com>
2018-01-26 12:02:54 +0300

    Implemented persistent image cache.

--- Scryfall/Classes/Cache/ImageCache.swift
+++ Scryfall/Classes/Cache/ImageCache.swift
@@ -16,8 +16,21 @@ import RxCocoa
  */
 class ImageDownloader {
     
-    /// Image cache.
-    private let imageCache = NSCache<NSString, UIImage>()
+    /// Cache directory.
+    private let cacheDir = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!.appendingPathComponent("cards", isDirectory: false)
+    
+    /**
+     * Removes all files from cache directory.
+     */
+    private func purge() {
+        do {
+            if FileManager.default.fileExists(atPath: cacheDir.path, isDirectory: nil) {
+                try FileManager.default.removeItem(at: cacheDir)
+            }
+        } catch {
+            print("failed to clear cache: \(error)")
+        }
+    }
     
     /**
      * Saves image for given URL in cache.
@@ -25,8 +38,18 @@ class ImageDownloader {
      * - parameter image: Image to save
      * - parameter url: Associated URL
      */
-    private func set(_ image: UIImage, for url: URL) {
-        imageCache.setObject(image, forKey: url.absoluteString as NSString)
+    private func set(_ image: NSData, for url: URL) {
+        func save(components: [String], currentPath: URL) {
+            let url = currentPath.appendingPathComponent(components.first!, isDirectory: false)
+            if (components.count == 1) {
+                FileManager.default.createFile(atPath: url.path, contents: image as Data, attributes: nil)
+            } else {
+                try! FileManager.default.createDirectory(at: url, withIntermediateDirectories: true, attributes: nil)
+                save(components: Array<String>(components.suffix(from: 1)), currentPath: url)
+            }
+        }
+        
+        save(components: url.pathComponents, currentPath: cacheDir)
     }
     
     /**
@@ -35,8 +58,18 @@ class ImageDownloader {
      * - parameter url: Associated URL
      * - returns: Image associated with given URL or nil if such image isn't found
      */
-    private func cachedImage(for url: URL) -> UIImage? {
-        return imageCache.object(forKey: url.absoluteString as NSString)
+    private func cachedImage(for url: URL) -> NSData? {
+        do {
+            let fileUrl = cacheDir.appendingPathComponent(url.path, isDirectory: false)
+            if FileManager.default.fileExists(atPath: fileUrl.path) {
+                let data = try Data(contentsOf: fileUrl)
+                return data as NSData
+            }
+        } catch {
+            print("failed to load cached image for \(url)")
+        }
+        
+        return nil
     }
     
     /**
@@ -47,14 +80,13 @@ class ImageDownloader {
      */
     func image(for url: URL) -> Observable<UIImage?> {
         if let cachedImage = cachedImage(for: url) {
-            return Observable.just(cachedImage)
+            return Observable.just(UIImage(data: cachedImage as Data))
         }
         
         // Download an image, save it into cache, and pass along to the subscriber.
-        return URLSession.shared.rx.data(request: URLRequest(url: url)).map(UIImage.init(data:)).do(onNext: { image in
-                if let image = image {
-                    self.set(image, for: url)
-                }
+        return URLSession.shared.rx.data(request: URLRequest(url: url)).do(onNext: { image in
+                self.set(image as NSData, for: url)
             })
+            .map { return UIImage(data: $0 as Data) }
     }
 }
